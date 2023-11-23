e97c0e2dad95ec321bf2f95944999bde7800ad18
tarasov1812 <tarasov1812@gmail.com>
2023-11-23 12:52:39 +0100

    version for presentation

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.post('/api/send-prompt', async (req, res) => {
       },
       {
         headers: {
-          'Authorization': `Bearer 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`, // Replace with your API key
+          'Authorization': `Bearer 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`, // Replace with your API key
           'Content-Type': 'application/json',
         },
       }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -43,37 +43,26 @@ function Tasks() {
     setPrompt2(event.target.value);
   };
 
+  const handleCheckboxChange = (e) => {
+    const { value, checked } = e.target;
+    if (checked) {
+      setSelectedOption(prevSelectedOptions => [...prevSelectedOptions, value]);
+    } else {
+      setSelectedOption(prevSelectedOptions =>
+        prevSelectedOptions.filter(option => option !== value)
+      );
+    }
+  };
+
   const handleSubmit = async () => {
     setResponse('');
     setResponse2('');
     try {
-      let finalText = '';
-      let finalText2 = '';
-    console.log(selectedOption);
-      switch (selectedOption) {
-        case 'Grammar Check':
-          finalText = 'Please correct any grammar mistakes in the following sentence: "';
-          finalText2 = 'Identify and list any grammatically incorrect words in the following sentence: "';
-          break;
-        case 'Political restrictions':
-          finalText = 'Please eliminate in the following sentence only political uncorrected sentences: "';
-          finalText2 = 'Please correct the following sentence while considering political restrictions: "';
-          break;
-        case 'Profanity Check':
-          finalText = 'Please check the following sentence for profanity and provide a version without any inappropriate language: "';
-          finalText2 = 'Identify and list any profanity words in the following sentence: "';
-          break;
-        case 'Others':
-          finalText = 'Replace word/words like ' + prompt2 + ' with the synonims in the sentence: "';
-          finalText2 = 'Check and list how many times word/words like ' + prompt2 + ' in the sentence: "';
-          break;
-        default:
-          finalText = 'Please select an option';
-          break;
-      }
-
-    //   setResponse(finalText);
-    console.log(finalText);
+      // let finalText = 'Please correct any grammar mistakes in the following sentence and Please eliminate in the following sentence only political uncorrected sentences "';
+      // let finalText2 = 'Identify and list any grammatically incorrect words in the following sentence and Please eliminate in the following sentence only political uncorrected sentences and make the list of errors"';
+      let finalText = '"Correct grammar mistakes and political uncorrected sentences"';
+      let finalText2 = '"Make the list of grammar error and politically incorrect context in this text"'
+    
     let text = finalText + prompt + '"';
     console.log(text);
       const res = await axios.post('/api/send-prompt', {
@@ -81,7 +70,7 @@ function Tasks() {
       });
 
       setResponse(res.data.completion);
-    let text2 = finalText2 + prompt + '"';
+    let text2 = ' ' + prompt + ' what problems does this text has? write down the points';
     console.log(text2);
     const res2 = await axios.post('/api/send-prompt', {
       prompt: text2,
@@ -91,7 +80,6 @@ function Tasks() {
     } catch (error) {
       console.error('Error:', error);
     }
-    filterWords();
   };
 
 
@@ -102,7 +90,7 @@ function Tasks() {
           <div className={styles.content}>
             
             <div className={styles.task}>
-            <input type="text" placeholder="Title project" className="input input-bordered mr-10" />
+            <input type="text" placeholder="Title" className="input input-bordered mr-10" />
             <select className="select select-bordered ml-20 w-100">
                 <option disabled selected>Select your project</option>
                 <option>Project 1</option>
@@ -123,33 +111,56 @@ function Tasks() {
             </button>
             <div className={styles.empty}/>
             <div className="form-control">
-            <label className="label cursor-pointer">
-                <span className="label-text">Grammar Check</span> 
-                <input type="radio" name="radio-10" className="radio checked:bg-red-500" checked={selectedOption === 'Grammar Check'}
-                  value="Grammar Check"
-                  onChange={(e) => setSelectedOption(e.target.value)} 
-                />
-            </label>
-            </div>
+  <label className="label cursor-pointer">
+    <span className="label-text">Grammar Check</span> 
+    <input 
+      type="checkbox" 
+      name="checkbox-3" 
+      className="checkbox checkbox-info" 
+      checked={selectedOption.includes('Grammar Check')}
+      value="Grammar Check"
+      onChange={handleCheckboxChange} 
+    />
+  </label>
+</div>
             <div className="form-control">
-            <label className="label cursor-pointer">
-                <span className="label-text">Political restrictions</span> 
-                <input type="radio" name="radio-10" className="radio checked:bg-blue-500" checked={selectedOption === 'Political restrictions'}
-                  value="Political restrictions"
-                  onChange={(e) => setSelectedOption(e.target.value)} />
-            </label>
-            <label className="label cursor-pointer">
-                <span className="label-text">Profanity Check</span> 
-                <input type="radio" name="radio-10" className="radio checked:bg-red-500" checked={selectedOption === 'Profanity Check'}
-                  value="Profanity Check"
-                  onChange={(e) => setSelectedOption(e.target.value)} />
-            </label>
-            <label className="label cursor-pointer">
-                <span className="label-text">Others</span> 
-                <input type="radio" name="radio-10" className="radio checked:bg-blue-500" checked={selectedOption === 'Others'}
-                  value="Others"
-                  onChange={(e) => setSelectedOption(e.target.value)} />
-            </label>
+              <label className="label cursor-pointer">
+    <span className="label-text">Political restrictions</span> 
+    <input 
+      type="checkbox" 
+      name="checkbox-2" 
+      className="checkbox checkbox-info" 
+      checked={selectedOption.includes('Political restrictions')}
+      value="Political restrictions"
+      onChange={handleCheckboxChange} 
+    />
+  </label>
+</div>
+<div className="form-control">
+  <label className="label cursor-pointer">
+    <span className="label-text">Profanity Check</span> 
+    <input 
+      type="checkbox" 
+      name="checkbox-3" 
+      className="checkbox checkbox-info" 
+      checked={selectedOption.includes('Profanity Check')}
+      value="Profanity Check"
+      onChange={handleCheckboxChange} 
+    />
+  </label>
+</div>
+<div className="form-control">
+  <label className="label cursor-pointer">
+    <span className="label-text">Others</span> 
+    <input 
+      type="checkbox" 
+      name="checkbox-4" 
+      className="checkbox checkbox-info" 
+      checked={selectedOption.includes('Others')}
+      value="Others"
+      onChange={handleCheckboxChange} 
+    />
+  </label>
             <input type="text" placeholder="input your restricion here" className="input input-bordered w-full max-w-xs mb-10" onChange={handleInputChange2}/>
             </div>
             </div>
